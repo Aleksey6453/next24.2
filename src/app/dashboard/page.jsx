1bf422dc68c2c174972ba834cc46bdd6ce0d8cf1
Aleksey6453 <login6453@gmail.com>
2024-03-27 00:04:15 +0200

    add swr

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+'use client'
 
-export const metadata = {
-  title: 'Dashboard',
-  description: 'Dashboard page',
-}
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+// export const metadata = {
+//   title: 'Dashboard',
+//   description: 'Dashboard page',
+// }
 
 const page = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(()=>{
+    const getData = async()=>{
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+
+      if (!res.ok) {
+        setError(true)
+      }
+      setLoading(true)
+
+      const data = await res.json()
+      setData(data)
+      setLoading(false)
+    }
+    getData()
+  }, [])
+
+  console.log(data)
+
+
   return (
     <div className='wrapPage'>
       <h1 className='title'>
