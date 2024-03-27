a415a6a35af9843a9d9bcd228bda6a1575e3327d
Aleksey6453 <login6453@gmail.com>
2024-03-28 00:26:43 +0200

    add SWR work version

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { useState, useEffect } from 'react'
+import useSWR from 'swr'
 
 // export const metadata = {
 //   title: 'Dashboard',
@@ -9,28 +10,32 @@ import { useState, useEffect } from 'react'
 // }
 
 const page = () => {
-  const [data, setData] = useState([])
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+  // const [data, setData] = useState([])
+  // const [error, setError] = useState(false)
+  // const [loading, setLoading] = useState(false)
 
-  useEffect(()=>{
-    const getData = async()=>{
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
+  // useEffect(()=>{
+  //   const getData = async()=>{
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: 'no-store' })
 
-      if (!res.ok) {
-        setError(true)
-      }
-      setLoading(true)
+  //     if (!res.ok) {
+  //       setError(true)
+  //     }
+  //     setLoading(true)
 
-      const data = await res.json()
-      setData(data)
-      setLoading(false)
-    }
-    getData()
-  }, [])
+  //     const data = await res.json()
+  //     setData(data)
+  //     setLoading(false)
+  //   }
+  //   getData()
+  // }, [])
+
+  // console.log(data)
+
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
   console.log(data)
-
 
   return (
     <div className='wrapPage'>
